53831ea2e2c4ba0769830c46261ae1bdf045e7e7
acekamaa <kvnkamaa@gmail.com>
2024-07-03 23:12:44 +0300

    0. Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 #creating a file with the following properties
 file { '/tmp/school':
-        mode    => '0744',
-        owner   => 'www-data',
-        group   => 'www-data',
-        content => 'I love Puppet'
-    }
+  ensure  => 'file',
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
